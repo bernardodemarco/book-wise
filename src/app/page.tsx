8f9dcbe98a29f1b346f0a286ef0512a6c6294bcc
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-07-30 21:26:24 -0300

    set tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div></div>
+  return (
+    <>
+      <h1 className="text-2xl text-green-300 font-bold">Book wise</h1>
+      <div className="w-10 h-10 bg-gradient-horizontal"></div>
+      <div className="mt-6 w-10 h-10 bg-gradient-vertical"></div>
+    </>
+  )
 }

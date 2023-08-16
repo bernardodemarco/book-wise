90e22a4fbbf20eff7d5dca8e0f1fd568009670d8
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-08-16 17:06:57 -0300

    chore: add prettier integration with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-300 font-bold">Book wise</h1>
-      <div className="w-10 h-10 bg-gradient-horizontal"></div>
-      <div className="mt-6 w-10 h-10 bg-gradient-vertical"></div>
+      <h1 className="text-2xl font-bold text-green-300">Book wise</h1>
+      <div className="h-10 w-10 bg-gradient-horizontal"></div>
+      <div className="mt-6 flex h-10 w-10 items-center justify-center bg-gradient-vertical"></div>
     </>
   )
 }

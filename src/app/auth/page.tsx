74948ea5943128bbfe36e4d29cb6b614337c2b97
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-08-28 18:04:46 -0300

    feat: authpage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,6 @@
 import { GithubIcon } from '@/assets/icons/GithubIcon'
 import { GoogleIcon } from '@/assets/icons/GoogleIcon'
+import { GuestIcon } from '@/assets/icons/GuestIcon'
 import Image from 'next/image'
 import authImage from 'public/auth-image.png'
 
@@ -7,21 +8,27 @@ export default function AuthPage() {
   return (
     <div className="flex-1 p-5">
       <Image src={authImage} alt="" className="hidden" />
-      <section className="flex min-h-full flex-col items-center justify-center">
-        <strong className="text-xl font-bold text-gray-100">
-          Boas vindas!
-        </strong>
-        <p className="text-base font-normal text-gray-200">
-          Faça seu login ou acesse como visitante.
-        </p>
-        <div>
-          <button className="flex items-center gap-5 rounded-lg bg-gray-600 px-6 py-5 text-lg font-bold text-gray-200">
+      <section className="flex min-h-full flex-col items-center justify-center gap-10">
+        <div className="flex flex-col items-center gap-1">
+          <strong className="text-xl font-bold text-gray-100">
+            Boas vindas!
+          </strong>
+          <p className="text-base font-normal text-gray-200">
+            Faça seu login ou acesse como visitante.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <button className="flex w-80 max-w-[85vw] items-center gap-5 rounded-lg bg-gray-600 px-6 py-5 text-lg font-bold text-gray-200">
             <GoogleIcon />
             Entrar com Google
           </button>
-          <button className="flex items-center gap-5 rounded-lg bg-gray-600 px-6 py-5 text-lg font-bold text-gray-200">
+          <button className="flex w-80 max-w-[85vw] items-center gap-5 rounded-lg bg-gray-600 px-6 py-5 text-lg font-bold text-gray-200">
             <GithubIcon />
             Entrar com Github
+          </button>
+          <button className="flex w-80 max-w-[85vw] items-center gap-5 rounded-lg bg-gray-600 px-6 py-5 text-lg font-bold text-gray-200">
+            <GuestIcon />
+            Entrar como visitante
           </button>
         </div>
       </section>

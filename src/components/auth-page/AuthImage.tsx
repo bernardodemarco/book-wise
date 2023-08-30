c95522c0c49eee49e872e90794afadb96d4d2186
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-08-30 11:30:37 -0300

    fix image width

--- src/components/auth-page/AuthImage.tsx
+++ src/components/auth-page/AuthImage.tsx
@@ -1,14 +1,17 @@
-import { BookWiseLogo } from '@/assets/icons/BookWiseLogo'
 import Image from 'next/image'
 import authImage from 'public/auth-image.png'
+import { BookWiseLogo } from '@/assets/icons/BookWiseLogo'
 
 export function AuthImage() {
   return (
     <div className="bg-background-gradient relative hidden w-[600px] overflow-hidden rounded-xl lg:block">
       <Image
         src={authImage}
-        alt=""
-        className="h-authImageHeight absolute object-cover opacity-20"
+        alt="Imagem de uma mulher deitada em um sofá lendo um livro"
+        className="h-absolute object-cover opacity-20"
+        fill={true}
+        sizes="(max-width: 1023px) 0px, 600px"
+        priority={true}
       />
       <BookWiseLogo className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2" />
     </div>

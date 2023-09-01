e5a0e4e24a78e67088def2489f0afbd68ff2f4cb
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-09-01 10:54:59 -0300

    add column gap to app grid

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import { Sidebar } from '@/components/Sidebar'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-app p-5">
+    <div className="grid grid-cols-app gap-x-24 p-5">
       <Sidebar />
       <main>{children}</main>
     </div>

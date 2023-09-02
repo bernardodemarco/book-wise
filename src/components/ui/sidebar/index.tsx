f7e7ed7d080961f66782cc17d2181da0b8303d58
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-09-02 15:26:59 -0300

    add styles on hover and on active links

--- src/components/ui/sidebar/index.tsx
+++ src/components/ui/sidebar/index.tsx
@@ -1,5 +1,4 @@
 import { BookWiseLogo } from '@/assets/icons/BookWiseLogo'
-import { Glasses, TrendingUp, User2 } from 'lucide-react'
 import { NavLink } from './nav-link'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
@@ -15,15 +14,9 @@ export async function Sidebar() {
     <aside className="flex h-screen-height flex-col items-center rounded-xl bg-gray-800 pb-6 pt-10 sm:bg-gray-700">
       <BookWiseLogo className="mb-14 hidden h-8 w-32 md:block" />
       <nav className="flex flex-col items-start gap-6">
-        <NavLink slug="">
-          <TrendingUp className="h-4 w-4 text-gray-400 sm:h-6 sm:w-6" />
-        </NavLink>
-        <NavLink slug="explore">
-          <Glasses className="h-4 w-4 text-gray-400 sm:h-6 sm:w-6" />
-        </NavLink>
-        <NavLink slug="profile">
-          <User2 className="h-4 w-4 text-gray-400 sm:h-6 sm:w-6" />
-        </NavLink>
+        <NavLink slug="" />
+        <NavLink slug="explore" />
+        <NavLink slug="profile" />
       </nav>
       {session ? (
         <LogoutButton avatarURL={avatarURL} username={username} />
--- src/components/ui/sidebar/nav-link.tsx
+++ src/components/ui/sidebar/nav-link.tsx
@@ -3,31 +3,39 @@
 import Link from 'next/link'
 import { useSelectedLayoutSegment } from 'next/navigation'
 import { getNavLinkTextOnSlug } from '@/utils/navLinkData'
-import { ReactNode } from 'react'
+import { Glasses, TrendingUp, User2 } from 'lucide-react'
 
 interface NavLinkProps {
   slug: '' | 'explore' | 'profile'
-  children: ReactNode
 }
 
-export function NavLink({ slug, children }: NavLinkProps) {
+export function NavLink({ slug }: NavLinkProps) {
   const currentPath = useSelectedLayoutSegment() ?? ''
   const isActive = currentPath === slug
   const navLinkText = getNavLinkTextOnSlug(slug)
 
+  const linkClass = isActive
+    ? "group mr-4 flex items-center gap-3 before:block before:h-4 before:w-1 before:rounded-full before:bg-gradient-vertical before:content-[''] sm:before:h-6 sm:before:w-1"
+    : "group mr-4 flex items-center gap-3 before:block before:h-4 before:w-1 before:rounded-full before:bg-gray-800 before:content-[''] hover:text-gray-100 sm:before:h-6 sm:before:w-1 sm:before:bg-gray-700"
+
+  const iconClass = isActive
+    ? 'h-4 w-4 text-gray-100 font-bold group-hover:text-gray-100 sm:h-6 sm:w-6'
+    : 'h-4 w-4 text-gray-400 group-hover:text-gray-100 group-hover:font-bold sm:h-6 sm:w-6'
+
+  const textClass = isActive
+    ? 'hidden text-base font-bold text-gray-100 md:block'
+    : 'hidden text-base text-gray-400 group-hover:text-gray-100 md:block'
+
   return (
-    <Link
-      href={`/${slug}`}
-      className={
-        isActive
-          ? "mr-4 flex items-center gap-3 before:block before:h-4 before:w-1 before:rounded-full before:bg-gradient-vertical before:content-[''] sm:before:h-6 sm:before:w-1"
-          : "mr-4 flex items-center gap-3 before:block before:h-4 before:w-1 before:rounded-full before:bg-gray-800 before:content-[''] sm:before:h-6 sm:before:w-1 sm:before:bg-gray-700"
-      }
-    >
-      {children}
-      <span className="hidden text-base font-bold text-gray-100 md:block">
-        {navLinkText}
-      </span>
+    <Link href={`/${slug}`} className={linkClass}>
+      {slug === '' ? (
+        <TrendingUp className={iconClass} />
+      ) : slug === 'explore' ? (
+        <Glasses className={iconClass} />
+      ) : (
+        <User2 className={iconClass} />
+      )}
+      <span className={textClass}>{navLinkText}</span>
     </Link>
   )
 }
